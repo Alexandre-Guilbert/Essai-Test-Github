f44fa81ac6889fdf80a9bca4c276c80d8e19bdcd
Alexandre-Guilbert <a.guilbert@labo-ve.fr>
2024-10-17 14:43:29 +0200

    premier envoi de documents

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const containerFond = document.getElementById("fond");
+
+
+
+function changeColor(){
+    if(containerFond.style.backgroundColor=="white")
+        containerFond.style.backgroundColor="black";
+    else 
+        containerFond.style.backgroundColor="red";
+      
+}
